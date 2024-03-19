3028ff277b09f4498f2238ee2a340e5c95d600a8
Aya <pyon220@gmail.com>
2024-03-18 22:30:08 -0700

    fixed margin

--- app/components/Tips.jsx
+++ app/components/Tips.jsx
@@ -155,14 +155,14 @@ export const Staff = ({ tipTotal }) => {
     <>
       <div className="mt-6">
         <div className="flex items-center mb-1">
-          <p className="text-[15px] md:text-[16px] w-[80px] ml-[55px] md:ml-[60px] leading-4 text-center break-words">
+          <p className="text-[15px] md:text-[16px] w-[80px] ml-[55px] md:ml-[60px] leading-3 text-center break-words">
             Hour{" "}
             <span className="text-[11px] leading-[5px]">
               <br />
               (30min →0.5)
             </span>
           </p>
-          <p className="text-[15px] md:text-[16px] w-[45px] ml-[4px] md:ml-[20px] leading-4 break-words flex flex-col items-center">
+          <p className="text-[15px] md:text-[16px] w-[45px] ml-[4px] md:ml-[20px] leading-3 break-words flex flex-col items-center">
             Break
             <span className="text-[11px] leading-[5px]">
               <br />
