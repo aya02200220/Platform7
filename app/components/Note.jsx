72f7ab28d734349df886432144f9e22ee9cba2ca
Aya <pyon220@gmail.com>
2024-04-08 17:31:59 -0700

    fixed note time

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -8,7 +8,7 @@ export const Note = () => {
           <h3>Example</h3>
           <hr />
           <ul className="ml-5 list-disc">
-            <li>6:30am ~ 1:00pm → 5.5</li>
+            <li>6:30am ~ 1:00pm → 6.5</li>
             <li>8:00am ~ 1:30pm → 5.5</li>
             <li>8:00am ~ 3:00pm → 7.0</li>
             <li>1:00pm ~ 8:00pm → 7.0</li>
